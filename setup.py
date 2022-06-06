b1cea93960329a680379107a3747318b57a75266
Fabricio <fepafumi97@gmail.com>
2022-06-06 14:53:54 -0300

    build: :rocket: Creados archivos para deploy

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='traxpet-boosting',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+        'catboost',
+        'pydrda',
+        'pandas',
+        'sklearn',
+        'numpy'
+    ],
+)
